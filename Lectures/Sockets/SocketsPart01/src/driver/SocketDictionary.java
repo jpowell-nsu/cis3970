2b0a0b5336caf321060ad809487e3ba971886404
Jason Powell <powellja@nsula.edu>
2026-04-06 20:15:35 -0500

    updated filenames

--- Lectures/Sockets/SocketsPart01/src/driver/SocketDictionary.java
+++ Lectures/Sockets/SocketsPart01/src/driver/SocketDictionary.java
@@ -2,25 +2,34 @@ package driver;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class SocketDictionary {
 	
 	public static void main(String[] args) {
-		String host = "time.nist.gov";
-		int port = 13;	// well-know port for Daytime protocol
+		String host = "dict.org";
+		int port = 2628;
 		
 		try {
 			Socket socket = new Socket(host, port);
 			BufferedReader reader = new BufferedReader(
 					new InputStreamReader(socket.getInputStream())
 			);
+			PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
 			
-			String line;
-            while ((line = reader.readLine()) != null) {
-                System.out.println(line);
-            }
+			System.out.println("Server:" + reader.readLine());
             
+			String word = "example";
+			writer.println("DEFINE * " + word);
+			System.out.println("\nResponse:");
+            
+			String definition = reader.readLine(); // 250 = OK, end of response
+			while (!definition.startsWith("250")) {	
+                System.out.println(definition);
+                definition = reader.readLine();
+            }
+			
 			socket.close();
 			
 		} catch (Exception e) {
--- Lectures/Sockets/SocketsPart01/src/driver/SocketTime.java
+++ Lectures/Sockets/SocketsPart01/src/driver/SocketTime.java
@@ -2,34 +2,25 @@ package driver;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.Socket;
 
 public class SocketTime {
 	
 	public static void main(String[] args) {
-		String host = "dict.org";
-		int port = 2628;
+		String host = "time.nist.gov";
+		int port = 13;	// well-know port for Daytime protocol
 		
 		try {
 			Socket socket = new Socket(host, port);
 			BufferedReader reader = new BufferedReader(
 					new InputStreamReader(socket.getInputStream())
 			);
-			PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
 			
-			System.out.println("Server:" + reader.readLine());
-            
-			String word = "example";
-			writer.println("DEFINE * " + word);
-			System.out.println("\nResponse:");
-            
-			String definition = reader.readLine(); // 250 = OK, end of response
-			while (!definition.startsWith("250")) {	
-                System.out.println(definition);
-                definition = reader.readLine();
+			String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
             }
-			
+            
 			socket.close();
 			
 		} catch (Exception e) {
